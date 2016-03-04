a0bb9e90555fd11c149ebc4de40bf229f79e0b9b
Billy Saysavath <bisaysavath@gmail.com>
2016-03-04 15:19:52 -0800

    Add app.js

---
+++ javascripts/app.js
@@ -0,0 +1,13 @@
+var main = function () {
+  $("main h2").hide();
+  $("main .user-info p").hide();
+
+  $("main h2").fadeIn( function () {
+    $("main .user-info p").slideDown(1000, function () {
+      console.log("hi");
+    });
+  });
+
+}
+
+$(document).ready(main);
