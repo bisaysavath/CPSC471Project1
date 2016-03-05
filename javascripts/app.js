612d2d6a556ddda19d9c68cc6fb094d6addcc8cb
Billy Saysavath <bisaysavath@gmail.com>
2016-03-04 18:11:50 -0800

    Fix style problems

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,6 +6,13 @@ var main = function () {
     $("main .user-info p").slideDown(1000);
   });
 
+  // $("body main form button").on("click", function () {
+  //   // console.log($(".form-name").val());
+  //   // console.log($(".form-email").val());
+  //   // console.log($(".form-phone").val());
+  //   // console.log($(".form-msg").val());
+  //   alert($(".form-name").val());
+  // })
 }
 
 $(document).ready(main);
