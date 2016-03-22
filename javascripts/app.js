d00cb685ff56b746dd77ac6e82ee61478a43e3f1
Billy Saysavath <bisaysavath@gmail.com>
2016-03-22 10:31:07 -0700

    Revert "Useless commit"
    This reverts commit 863489b7c1e4d007e28f9d30f78d99ed7547bdab.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,12 @@
 var main = function () {
     "use strict";
-
+    
     $("h2").hide();
+    $("main .user-info p").hide();
 
-    $("h2").fadeIn();
+    $("h2").fadeIn( function () {
+    $("main .user-info p").slideDown(1000);
+    });
 }
 
 $(document).ready(main);
